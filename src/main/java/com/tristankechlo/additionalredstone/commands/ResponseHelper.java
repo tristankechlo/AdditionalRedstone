bd868adbb602c3b84c2973ce0758b491eb7f5835
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2022-12-09 01:41:38 +0100

    added command `/additionalredstone`

---
+++ src/main/java/com/tristankechlo/additionalredstone/commands/ResponseHelper.java
@@ -0,0 +1,32 @@
+package com.tristankechlo.additionalredstone.commands;
+
+import com.tristankechlo.additionalredstone.AdditionalRedstone;
+import net.minecraft.ChatFormatting;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+
+public final class ResponseHelper {
+
+    public static MutableComponent start() {
+        return Component.literal("[" + AdditionalRedstone.MOD_NAME + "] ").withStyle(ChatFormatting.GOLD);
+    }
+
+    public static void sendMessage(CommandSourceStack source, Component message, boolean broadcastToOps) {
+        MutableComponent start = start().append(message);
+        source.sendSuccess(start, broadcastToOps);
+    }
+
+    public static MutableComponent clickableLink(String url, String displayText) {
+        MutableComponent mutableComponent = Component.literal(displayText);
+        mutableComponent.withStyle(ChatFormatting.GREEN, ChatFormatting.UNDERLINE);
+        mutableComponent.withStyle(style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url)));
+        return mutableComponent;
+    }
+
+    public static MutableComponent clickableLink(String url) {
+        return clickableLink(url, url);
+    }
+
+}
