4c994c1b50e3a5e17dd3bd47f3c35b0ffdef5c03
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2022-12-22 20:46:54 +0100

    refactor ModBlocks

--- src/main/java/com/tristankechlo/additionalredstone/init/ModBlocks.java
+++ src/main/java/com/tristankechlo/additionalredstone/init/ModBlocks.java
@@ -12,9 +12,9 @@ public class ModBlocks {
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, AdditionalRedstone.MOD_ID);
 
-    public static final RegistryObject<Block> CIRCUIT_MAKER_BLOCK = BLOCKS.register("circuit_maker", () -> new CircuitMakerBlock());
-    public static final RegistryObject<Block> OSCILLATOR_BLOCK = BLOCKS.register("oscillator", () -> new OscillatorBlock());
-    public static final RegistryObject<Block> TIMER_BLOCK = BLOCKS.register("timer", () -> new TimerBlock());
+    public static final RegistryObject<Block> CIRCUIT_MAKER_BLOCK = BLOCKS.register("circuit_maker", CircuitMakerBlock::new);
+    public static final RegistryObject<Block> OSCILLATOR_BLOCK = BLOCKS.register("oscillator", OscillatorBlock::new);
+    public static final RegistryObject<Block> TIMER_BLOCK = BLOCKS.register("timer", TimerBlock::new);
 
     public static final RegistryObject<Block> AND_GATE_BLOCK = BLOCKS.register("and_gate", () -> new ThreeInputLogicGate(ThreeInputLogic::and));
     public static final RegistryObject<Block> NAND_GATE_BLOCK = BLOCKS.register("nand_gate", () -> new ThreeInputLogicGate(ThreeInputLogic::nand));
@@ -22,12 +22,12 @@ public class ModBlocks {
     public static final RegistryObject<Block> NOR_GATE_BLOCK = BLOCKS.register("nor_gate", () -> new ThreeInputLogicGate(ThreeInputLogic::nor));
     public static final RegistryObject<Block> XOR_GATE_BLOCK = BLOCKS.register("xor_gate", () -> new ThreeInputLogicGate(ThreeInputLogic::xor));
     public static final RegistryObject<Block> XNOR_GATE_BLOCK = BLOCKS.register("xnor_gate", () -> new ThreeInputLogicGate(ThreeInputLogic::xnor));
-    public static final RegistryObject<Block> NOT_GATE_BLOCK = BLOCKS.register("not_gate", () -> new NotGateBlock());
+    public static final RegistryObject<Block> NOT_GATE_BLOCK = BLOCKS.register("not_gate", NotGateBlock::new);
 
-    public static final RegistryObject<Block> T_FLIP_FLOP_BLOCK = BLOCKS.register("t_flip_flop", () -> new TFlipFlopBlock());
-    public static final RegistryObject<Block> TOGGLE_LATCH_BLOCK = BLOCKS.register("toggle_latch", () -> new ToggleLatchBlock());
-    public static final RegistryObject<Block> SR_LATCH_BLOCK = BLOCKS.register("sr_latch", () -> new SRLatchBlock());
-    public static final RegistryObject<Block> RS_LATCH_BLOCK = BLOCKS.register("rs_latch", () -> new RSLatchBlock());
-    public static final RegistryObject<Block> SEQUENCER_BLOCK = BLOCKS.register("sequencer", () -> new SequencerBlock());
+    public static final RegistryObject<Block> T_FLIP_FLOP_BLOCK = BLOCKS.register("t_flip_flop", TFlipFlopBlock::new);
+    public static final RegistryObject<Block> TOGGLE_LATCH_BLOCK = BLOCKS.register("toggle_latch", ToggleLatchBlock::new);
+    public static final RegistryObject<Block> SR_LATCH_BLOCK = BLOCKS.register("sr_latch", SRLatchBlock::new);
+    public static final RegistryObject<Block> RS_LATCH_BLOCK = BLOCKS.register("rs_latch", RSLatchBlock::new);
+    public static final RegistryObject<Block> SEQUENCER_BLOCK = BLOCKS.register("sequencer", SequencerBlock::new);
 
 }
