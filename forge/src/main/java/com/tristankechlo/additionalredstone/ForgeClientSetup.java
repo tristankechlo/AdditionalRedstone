af232feeeaa3aed1d6c469d90f417f74fdec7f77
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2025-01-18 20:32:12 +0100

    fix block render type

--- forge/src/main/java/com/tristankechlo/additionalredstone/ForgeClientSetup.java
+++ forge/src/main/java/com/tristankechlo/additionalredstone/ForgeClientSetup.java
@@ -1,8 +1,11 @@
 package com.tristankechlo.additionalredstone;
 
 import com.tristankechlo.additionalredstone.client.screen.CircuitMakerScreen;
+import com.tristankechlo.additionalredstone.init.ModBlocks;
 import com.tristankechlo.additionalredstone.init.ModContainer;
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,6 +17,25 @@ public class ForgeClientSetup {
     @SubscribeEvent
     public static void init(final FMLClientSetupEvent event) {
         MenuScreens.register(ModContainer.CIRCUIT_MAKER_CONTAINER.get(), CircuitMakerScreen::new);
+
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.CIRCUIT_MAKER_BLOCK.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.OSCILLATOR_BLOCK.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.TIMER_BLOCK.get(), RenderType.cutout());
+
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.AND_GATE_BLOCK.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.NAND_GATE_BLOCK.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.OR_GATE_BLOCK.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.NOR_GATE_BLOCK.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.XOR_GATE_BLOCK.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.XNOR_GATE_BLOCK.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.NOT_GATE_BLOCK.get(), RenderType.cutout());
+
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.T_FLIP_FLOP_BLOCK.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.TOGGLE_LATCH_BLOCK.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.SR_LATCH_BLOCK.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.RS_LATCH_BLOCK.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.SEQUENCER_BLOCK.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.SUPERGATE_BLOCK.get(), RenderType.cutout());
     }
 
 }
