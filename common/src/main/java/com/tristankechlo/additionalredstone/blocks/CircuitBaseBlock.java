2a728163ecb27a38abf84ab00ef224343f14b724
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2025-01-18 20:32:37 +0100

    CircuitBase no longer placeable on top of each other

--- common/src/main/java/com/tristankechlo/additionalredstone/blocks/CircuitBaseBlock.java
+++ common/src/main/java/com/tristankechlo/additionalredstone/blocks/CircuitBaseBlock.java
@@ -2,6 +2,7 @@ package com.tristankechlo.additionalredstone.blocks;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -14,6 +15,11 @@ public class CircuitBaseBlock extends Block {
 
     public CircuitBaseBlock() {
         super(Properties.copy(Blocks.REPEATER));
+    }
+
+    @Override
+    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        return canSupportRigidBlock(level, pos.below());
     }
 
     @Override
