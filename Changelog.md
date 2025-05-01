3420e9d42155c40cd8659c47c72c85cb9cb2dd01
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2025-05-01 22:16:00 +0200

    (fix): light detector being recognized as fuel in the furnace

--- Changelog.md
+++ Changelog.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## Version 1.20.4 - 1.2.3
+
+- fix light detector being recognized as fuel in the furnace
+
 ## Version 1.20.4 - 1.2.2
 
 - port to 1.20.4
--- forge/src/main/java/com/tristankechlo/additionalredstone/ForgeAdditionalRedstone.java
+++ forge/src/main/java/com/tristankechlo/additionalredstone/ForgeAdditionalRedstone.java
@@ -1,9 +1,11 @@
 package com.tristankechlo.additionalredstone;
 
 import com.tristankechlo.additionalredstone.commands.ProjectLinks;
+import com.tristankechlo.additionalredstone.init.ModItems;
 import com.tristankechlo.additionalredstone.platform.ForgePacketHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(AdditionalRedstone.MOD_ID)
@@ -18,10 +20,17 @@ public class ForgeAdditionalRedstone {
 
         //register listener
         MinecraftForge.EVENT_BUS.addListener(this::registerCommands);
+        MinecraftForge.EVENT_BUS.addListener(this::getBurnTime);
     }
 
     private void registerCommands(RegisterCommandsEvent event) {
         ProjectLinks.registerAsCommand(event.getDispatcher(), event.getBuildContext(), event.getCommandSelection());
+    }
+
+    private void getBurnTime(final FurnaceFuelBurnTimeEvent event) {
+        if (event.getItemStack().is(ModItems.LIGHT_DETECTOR_ITEM.get())) {
+            event.setBurnTime(300);
+        }
     }
 
 }
