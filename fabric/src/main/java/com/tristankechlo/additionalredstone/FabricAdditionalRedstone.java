f84868158ff7c74d8c04f953aa79681217f1848c
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2025-05-01 22:29:48 +0200

    (fix): light detector not burnable in furnace

--- fabric/src/main/java/com/tristankechlo/additionalredstone/FabricAdditionalRedstone.java
+++ fabric/src/main/java/com/tristankechlo/additionalredstone/FabricAdditionalRedstone.java
@@ -1,9 +1,11 @@
 package com.tristankechlo.additionalredstone;
 
 import com.tristankechlo.additionalredstone.commands.ProjectLinks;
+import com.tristankechlo.additionalredstone.init.ModItems;
 import com.tristankechlo.additionalredstone.platform.FabricPacketHelper;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 
 public class FabricAdditionalRedstone implements ModInitializer {
 
@@ -17,6 +19,9 @@ public class FabricAdditionalRedstone implements ModInitializer {
 
         //register mod command
         CommandRegistrationCallback.EVENT.register(ProjectLinks::registerAsCommand);
+
+        // make light detector item a fuel in the furnace
+        FuelRegistry.INSTANCE.add(ModItems.LIGHT_DETECTOR_ITEM.get(), 300);
     }
 
 }
--- neoforge/src/main/java/com/tristankechlo/additionalredstone/NeoForgeAdditionalRedstone.java
+++ neoforge/src/main/java/com/tristankechlo/additionalredstone/NeoForgeAdditionalRedstone.java
@@ -1,11 +1,13 @@
 package com.tristankechlo.additionalredstone;
 
 import com.tristankechlo.additionalredstone.commands.ProjectLinks;
+import com.tristankechlo.additionalredstone.init.ModItems;
 import com.tristankechlo.additionalredstone.platform.NeoForgePacketHelper;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.neoforged.neoforge.event.furnace.FurnaceFuelBurnTimeEvent;
 
 @Mod(AdditionalRedstone.MOD_ID)
 public class NeoForgeAdditionalRedstone {
@@ -19,10 +21,17 @@ public class NeoForgeAdditionalRedstone {
 
         //register listener
         NeoForge.EVENT_BUS.addListener(this::registerCommands);
+        NeoForge.EVENT_BUS.addListener(this::getBurnTime);
     }
 
     private void registerCommands(RegisterCommandsEvent event) {
         ProjectLinks.registerAsCommand(event.getDispatcher(), event.getBuildContext(), event.getCommandSelection());
+    }
+
+    private void getBurnTime(final FurnaceFuelBurnTimeEvent event) {
+        if (event.getItemStack().is(ModItems.LIGHT_DETECTOR_ITEM.get())) {
+            event.setBurnTime(300);
+        }
     }
 
 }
